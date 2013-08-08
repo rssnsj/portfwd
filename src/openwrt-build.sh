8d76fadb55bd3fc209f83133d7216f28f883f35a
rssn@163.com <rssn@163.com@9c28ceb0-2b5c-c2bc-5ac4-01e21b9a1ca7>
2013-08-08 15:08:44 +0000

    1. openwrt-build.sh, Makefile: Fixed for OpenWrt building, libevent paths added; 2. socksnatd.c: fix comments and function names.

--- src/openwrt-build.sh
+++ src/openwrt-build.sh
@@ -1,3 +1,8 @@
 #!/bin/sh -x
-export STAGING_DIR=~/backfire-db120/staging_dir/toolchain-mips_gcc-4.3.3+cs_uClibc-0.9.30.1/bin
-make CROSS_COMPILE=$STAGING_DIR/mips-openwrt-linux-
+homedir=~
+export STAGING_DIR=$homedir/backfire-db120/staging_dir/toolchain-mips_gcc-4.3.3+cs_uClibc-0.9.30.1/bin
+
+make CROSS_COMPILE=$STAGING_DIR/mips-openwrt-linux- \
+	CFLAGS=-I$homedir/backfire-db120/build_dir/target-mips_uClibc-0.9.30.1/libevent-2.0.16-stable \
+	LDFLAGS=-L$homedir/backfire-db120/build_dir/target-mips_uClibc-0.9.30.1/libevent-2.0.16-stable/.libs
+
